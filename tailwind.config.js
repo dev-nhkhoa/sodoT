7e105389f256996dbef67380db3dff49f10cc698
nhkhoa.a <70632030+dev-nhkhoa@users.noreply.github.com>
2024-05-22 08:39:08 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{tsx,ts}', './components/**/*.{tsx,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
